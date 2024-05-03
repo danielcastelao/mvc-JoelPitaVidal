77159ba8016798d2c05e9d784c006f919851e4b6
dam1 <jpitavidal@danielcastelao.org>
2024-05-03 10:11:19 +0200

    configurando clase coches

--- src/cod/mvc/Coche.java
+++ src/cod/mvc/Coche.java
@@ -1,4 +1,36 @@
 package cod.mvc;
 
 public class Coche {
+    String matricula;
+    String modelo ;
+    Integer velocidad;
+
+    public Coche(String newMarticula, String newModelo,Integer newVelocidad) {
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public Integer getVelocidad() {
+        return velocidad;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public void setVelocidad(Integer velocidad) {
+        this.velocidad = velocidad;
+    }
 }
+
+
