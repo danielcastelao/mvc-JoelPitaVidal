04f21c8702c4e4f757160dd06d77fed8c547e17a
dam1 <jpitavidal@danielcastelao.org>
2024-05-03 10:11:07 +0200

    metodo para crear un coche

--- src/cod/mvc/Model.java
+++ src/cod/mvc/Model.java
@@ -1,4 +1,15 @@
 package cod.mvc;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Model {
+
+    private static ArrayList<Coche> parking= new ArrayList<>();
+
+    public static ArrayList<Coche> crearCoche (String newMatricula, String newModelo, Integer newVelocidad){
+    parking.add (new Coche (newMatricula , newModelo , newVelocidad));
+    return parking;
+
+    }
 }
