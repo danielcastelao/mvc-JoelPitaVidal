892fd0b2049482d698e418441eed074bda93cffc
dam1 <jpitavidal@danielcastelao.org>
2024-05-10 09:17:56 +0200

    Tests añadidos close #1

--- test/testCoches.java
+++ test/testCoches.java
@@ -10,11 +10,12 @@ public class testCoches {
 
     @Test
     public void comprobacionCrearCoche() {
-        Coche coche = Model.crearCoche("43656C35FD","Seat",0);
-        Assertions.assertEquals("43656C35FD",coche.matricula);
-        Assertions.assertEquals("Seat",coche.modelo);
+        Coche coche = Model.crearCoche("91765426LE","Cupra",0);
+        Assertions.assertEquals("91765426LE",coche.matricula);
+        Assertions.assertEquals("Cupra",coche.modelo);
         Assertions.assertEquals(0,coche.velocidad);
     }
+
 
     @Test
     public void comprobacionGetCoche(){
@@ -22,16 +23,19 @@ public class testCoches {
         Assertions.assertSame(coche,Model.getCoche("43656C35FC"));
     }
 
+
     @Test
     public void comprobacionCambiarVelocidad(){
         Coche coche = Model.crearCoche("43656C35FE","Seat",0);
         Assertions.assertEquals(24,Model.cambiarVelocidad("43656C35FE",24));
     }
 
+
     @Test
     public void getVelocidad(){
         Coche coche = Model.crearCoche("43656C35FR","Seat",45);
         Assertions.assertEquals(45,Model.getVelocidad("43656C35FR"));
     }
+
 
 }
